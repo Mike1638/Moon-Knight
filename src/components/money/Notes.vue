fc68e5b6e46733e028447dd503b22254fd16708d
hisoka1638 <1399952343@qq.com>
2022-04-02 23:27:15 +0800

    使用js实现支出和收入切换

---
+++ src/components/money/Notes.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" placeholder="在这里添加备注" />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang='scss' scoped>
+@import "~@/assets/style/helper.scss";
+.notes {
+  background: #f5f5f5;
+  display: flex;
+  font-size: 14px;
+  padding-left: 16px;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
---
+++ src/components/money/Types.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <ul class="types">
+      <li :class="type=== '-' && 'selected' " @click="selectType('-')">支出</li>
+      <li :class="type=== '+' && 'selected' " @click="selectType('+')">收入</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['xxx'],
+    mounted(){
+        console.log(this.xxx);
+    },
+    data(){
+        return {
+            type:'-'
+        }
+    },
+    methods:{
+        selectType(type){
+            if(type !== "-" && type !== "+"){
+                throw new Error('type is unknown')
+            }else{
+                this.type = type; 
+            }
+        }
+    }
+};
+</script>
+<style lang='scss' scoped>
+@import "~@/assets/style/helper.scss";
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      content: " ";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+</style>
