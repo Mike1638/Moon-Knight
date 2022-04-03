f54fee01294a0053098f4d6f9c24dc9e930ea37d
hisoka1638 <1399952343@qq.com>
2022-04-03 11:03:54 +0800

    暂时提交哦

--- src/components/money/Types.vue
+++ src/components/money/Types.vue
@@ -1,34 +1,37 @@
 <template>
   <div>
     <ul class="types">
-      <li :class="type=== '-' && 'selected' " @click="selectType('-')">支出</li>
-      <li :class="type=== '+' && 'selected' " @click="selectType('+')">收入</li>
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
     </ul>
   </div>
 </template>
 
-<script>
-export default {
-    props:['xxx'],
-    mounted(){
-        console.log(this.xxx);
-    },
-    data(){
-        return {
-            type:'-'
-        }
-    },
-    methods:{
-        selectType(type){
-            if(type !== "-" && type !== "+"){
-                throw new Error('type is unknown')
-            }else{
-                this.type = type; 
-            }
-        }
+<script lang="ts">
+import Vue from "vue";
+import { Component , Prop} from "vue-property-decorator";
+@Component
+export default class Types extends Vue {
+  type = "-";
+  @Prop(Number) xxx: number | undefined;
+  //Prop 告诉Vue xxx不是data 是Prop
+  //Number 告诉 Vue xxx 是个Number
+  // xxx 属性名
+  // number | undefined 就是 xxx的类型
+  selectType(type: string) {
+    if (type !== "-" && type !== "+") {
+      throw new Error("type is unknown");
+    } else {
+      this.type = type;
     }
-};
+  }
+  created(){
+      console.log(this.xxx);
+  }
+
+}
 </script>
+
 <style lang='scss' scoped>
 @import "~@/assets/style/helper.scss";
 .types {
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,7 @@
   <div>
     <Layout class-prefix="layout">
      <NumberPad/>
-     <Types xxx="hi"/>
+     <Types :xxx="132456" />
      <Notes/>
      <Tags/>
     </Layout>
