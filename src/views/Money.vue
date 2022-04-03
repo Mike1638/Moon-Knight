a05ca0fc16106c054131ccb519380f1abf7f4b2d
hisoka1638 <1399952343@qq.com>
2022-04-03 23:14:31 +0800

    添加时间信息 2022年4月3日23:16:25

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,28 +1,29 @@
 <template>
   <div>
     <Layout class-prefix="layout">
-      <NumberPad @update:value="onUpdateOK" />
-      <Types :value.sync='recode.type'  />
+      <NumberPad @update:value="onUpdateOK" @setrecodeList="setrecodeList" />
+      <Types :value.sync="recode.type" />
       <Notes @update:value="onUpdateNotes" />
       <Tags :datasourse.sync="tags" @update:value="onUpdateTags" />
-      {{recode}}
+      {{ recodeList }}
     </Layout>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
 import Layout from "@/components/Layout.vue";
+import Notes from "@/components/money/Notes.vue";
 import NumberPad from "@/components/money/NumberPad.vue";
 import Tags from "@/components/money/Tags.vue";
-import Notes from "@/components/money/Notes.vue";
 import Types from "@/components/money/Types.vue";
-import { Component } from "vue-property-decorator";
+import Vue from "vue";
+import { Component, Watch } from "vue-property-decorator";
 type Recode = {
   tags: string[];
   notes: string;
   type: string;
   amount: number;
+  createAt?:Date 
 };
 @Component({
   components: { Layout, NumberPad, Tags, Notes, Types },
@@ -34,6 +35,7 @@ export default class Money extends Vue {
     type: "-",
     amount: 0,
   };
+  recodeList: Recode[] = JSON.parse(window.localStorage.getItem('recodeList') || '[]')
   tags = ["衣", "食", "住", "行", "打游戏"];
   onUpdateTags(value: string[]) {
     this.recode.tags = value;
@@ -43,6 +45,17 @@ export default class Money extends Vue {
   }
   onUpdateOK(value: string) {
     this.recode.amount = parseFloat(value);
+  }
+  setrecodeList(){
+    const recode2:Recode = JSON.parse(JSON.stringify(this.recode))
+    recode2.createAt = new Date()
+    this.recodeList.push(recode2);
+   console.log(this.recodeList);
+   
+}
+  @Watch("recodeList")
+  onRecodeListchanged() {
+    window.localStorage.setItem("recodeList", JSON.stringify(this.recodeList));
   }
 }
 </script>
